c9bef66822e5c6105bd952ebabea8017eeeea874
Younes DRO <dro66_8@hotmail.fr>
2024-08-25 11:33:49 +0100

    test: Add PHPUnit test case for assigning a single contributor to a post

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -32,7 +32,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/rtcamp-contributors.php';
+	require dirname( __DIR__ ) . '/rtcamp-contributors.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
--- tests/test-class-rtcamp-contributors.php
+++ tests/test-class-rtcamp-contributors.php
@@ -10,23 +10,40 @@
  */
 class Test_RTCamp_Contributors extends WP_UnitTestCase {
 
-    /**
-     * Test assigning multiple contributors to a post.
-     */
-    public function test_assigning_multiple_contributors() {
+	/**
+	 * Test assigning multiple contributors to a post.
+	 */
+	public function test_assigning_multiple_contributors() {
 
-        $post_id = $this->factory->post->create();
+		$post_id = $this->factory->post->create();
 
-        $user1 = $this->factory->user->create(['role' => 'administrator']);
-        $user2 = $this->factory->user->create(['role' => 'author']);
-        $user3 = $this->factory->user->create(['role' => 'editor']); 
+		$user1 = $this->factory->user->create( array( 'role' => 'administrator' ) );
+		$user2 = $this->factory->user->create( array( 'role' => 'author' ) );
+		$user3 = $this->factory->user->create( array( 'role' => 'editor' ) );
 
-        update_post_meta($post_id, '_rtcamp_contributors', [$user1, $user2, $user3]);
-        
-        $contributors = get_post_meta($post_id, '_rtcamp_contributors', true);
+		update_post_meta( $post_id, '_rtcamp_contributors', array( $user1, $user2, $user3 ) );
 
-        $this->assertContains($user1, $contributors, 'User 1 should be a contributor.');
-        $this->assertContains($user2, $contributors, 'User 2 should be a contributor.');
-        $this->assertContains($user3, $contributors, 'User 3 should be a contributor.');
-    }
+		$contributors = get_post_meta( $post_id, '_rtcamp_contributors', true );
+
+		$this->assertContains( $user1, $contributors, 'User 1 should be a contributor.' );
+		$this->assertContains( $user2, $contributors, 'User 2 should be a contributor.' );
+		$this->assertContains( $user3, $contributors, 'User 3 should be a contributor.' );
+	}
+
+	/**
+	 * Test assigning a single contributor to a post.
+	 */
+	public function test_assigning_single_contributor() {
+
+		$post_id = $this->factory->post->create();
+
+		$user = $this->factory->user->create( array( 'role' => 'author' ) );
+
+		update_post_meta( $post_id, '_rtcamp_contributors', array( $user ) );
+
+		$contributors = get_post_meta( $post_id, '_rtcamp_contributors', true );
+
+		$this->assertCount( 1, $contributors, 'There should be exactly one contributor.' );
+		$this->assertEquals( $user, $contributors[0], 'The contributor should match the assigned user.' );
+	}
 }
