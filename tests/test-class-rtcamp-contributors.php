93d32384316f85d0b5fc26a8adc2a2dc05d94d7e
Younes DRO <dro66_8@hotmail.fr>
2024-08-25 11:58:27 +0100

    Add documentation and fix PHPCS alerts for test_displaying_contributors_box

--- tests/test-class-rtcamp-contributors.php
+++ tests/test-class-rtcamp-contributors.php
@@ -58,43 +58,58 @@ class Test_RTCamp_Contributors extends WP_UnitTestCase {
 		$this->assertEmpty( $contributors, 'There should be no contributors assigned to the post.' );
 	}
 
-    public function test_displaying_contributors_box() {
-        
-        $post_id = $this->factory->post->create(['post_content' => 'This is a test post.']);
-    
-        $user1 = $this->factory->user->create(['role' => 'author', 'display_name' => 'Author One']);
-        $user2 = $this->factory->user->create(['role' => 'editor', 'display_name' => 'Editor One']);
-        $user3 = $this->factory->user->create(['role' => 'administrator', 'display_name' => 'Admin One']);
-    
-        update_post_meta($post_id, '_rtcamp_contributors', [$user1, $user2, $user3]);
-    
-        global $post, $wp_query;
-        $post = get_post($post_id);
-        $wp_query = new WP_Query([
-            'p' => $post_id,
-            'post_type' => 'post',
-            'posts_per_page' => 1,
-        ]);
-        $wp_query->is_singular = true;
-        $wp_query->is_single = true;
-        $wp_query->is_page = false;
-        $wp_query->is_home = false;
-        $wp_query->is_archive = false;
-    
-        setup_postdata($post);
-    
-        $post_content = apply_filters('the_content', get_post_field('post_content', $post_id));
-    
+	/**
+	 * Test displaying the Contributors box at the end of post content.
+	 */
+	public function test_displaying_contributors_box() {
 
-        $this->assertStringContainsString('Contributors', $post_content, 'Contributors box should be present.');
-        $this->assertStringContainsString('Author One', $post_content, 'Author One should be listed as a contributor.');
-        $this->assertStringContainsString('Editor One', $post_content, 'Editor One should be listed as a contributor.');
-        $this->assertStringContainsString('Admin One', $post_content, 'Admin One should be listed as a contributor.');
-    
-        wp_reset_postdata();
-    }
-    
-    
-    
-    
+		$post_id = $this->factory->post->create( array( 'post_content' => 'This is a test post.' ) );
+
+		$user1 = $this->factory->user->create(
+			array(
+				'role'         => 'author',
+				'display_name' => 'Author One',
+			)
+		);
+		$user2 = $this->factory->user->create(
+			array(
+				'role'         => 'editor',
+				'display_name' => 'Editor One',
+			)
+		);
+		$user3 = $this->factory->user->create(
+			array(
+				'role'         => 'administrator',
+				'display_name' => 'Admin One',
+			)
+		);
+
+		update_post_meta( $post_id, '_rtcamp_contributors', array( $user1, $user2, $user3 ) );
+
+		global $post, $wp_query;
+		$post                  = get_post( $post_id );
+		$wp_query              = new WP_Query(
+			array(
+				'p'              => $post_id,
+				'post_type'      => 'post',
+				'posts_per_page' => 1,
+			)
+		);
+		$wp_query->is_singular = true;
+		$wp_query->is_single   = true;
+		$wp_query->is_page     = false;
+		$wp_query->is_home     = false;
+		$wp_query->is_archive  = false;
+
+		setup_postdata( $post );
+
+		$post_content = apply_filters( 'the_content', get_post_field( 'post_content', $post_id ) );
+
+		$this->assertStringContainsString( 'Contributors', $post_content, 'Contributors box should be present.' );
+		$this->assertStringContainsString( 'Author One', $post_content, 'Author One should be listed as a contributor.' );
+		$this->assertStringContainsString( 'Editor One', $post_content, 'Editor One should be listed as a contributor.' );
+		$this->assertStringContainsString( 'Admin One', $post_content, 'Admin One should be listed as a contributor.' );
+
+		wp_reset_postdata();
+	}
 }
